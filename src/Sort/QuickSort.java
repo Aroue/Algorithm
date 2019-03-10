04a8fa2d5f1b810df8860c378ef454098b6a8c3a
KLee <18290296998@163.com>
2019-03-10 22:09:00 +0800

    quickSort

--- src/Sort/QuickSort.java
+++ src/Sort/QuickSort.java
@@ -1,5 +1,30 @@
 package Sort;
 
 // 快速排序
-public class QuickSort {
+public class QuickSort extends Sort {
+
+    public static void sort(Comparable[] a)
+    {
+        sort(a, 0, a.length - 1);
+    }
+    private static void sort(Comparable[] a, int lo, int hi)
+    {
+        if (hi <= lo) return;
+        int j = partition(a, lo, hi); // 切分(请见“快速排序的切分”)
+        sort(a, lo, j-1);  // 将左半部分a[lo .. j-1]排序
+        sort(a, j+1, hi);  // 将右半部分a[j+1 .. hi]排序
+    }
+    public static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo, j = hi + 1;
+        Comparable v = a[i];
+        while (true) { // 扫描左右，检查扫描是否结束并交换元素
+            while (less(a[++i], v)) if (i == hi) break;
+            while (less(v, a[--j])) if (j == lo) break;
+            if (i >= j) break;
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+
+    }
 }
---
+++ src/Sort/Test.java
@@ -0,0 +1,22 @@
+package Sort;
+
+import java.io.File;
+
+import static FileUtil.ReadFileToArray.ReadFile;
+import static java.lang.System.currentTimeMillis;
+
+public class Test {
+
+    public static void main(String[] args) {
+        File file = new File("data.txt");
+        Comparable[] a = ReadFile(file);
+        long startTime = currentTimeMillis();
+        QuickSort.sort(a);
+        long endTime = currentTimeMillis();
+        for (Comparable intNumber: a) {
+            System.out.print(intNumber + " ");
+        }
+        System.out.println();
+        System.out.println(endTime - startTime + " ms");
+    }
+}
